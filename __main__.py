f11a8dbad2f878c413de9c89081fd10f93a66941
Andrew <andrew.fedosseyenko@gmail.com>
2024-04-27 15:33:57 +0500

    Done the info writing in the file

--- __main__.py
+++ __main__.py
@@ -55,66 +55,65 @@ class StockFrame(customtkinter.CTkFrame):
         main_label_font = customtkinter.CTkFont(family="Roboto", size=35, weight="bold")
         secondary_label_font = customtkinter.CTkFont(family="Roboto", size=12, weight="bold")
 
+        # The font color operation for the stock var's
         if stock_change > 0:
             actual_color = 'green'
         elif stock_change < 0:
             actual_color = 'red'
         else:
-            actual_color = 'white' #### Later it will be changed every tick
+            actual_color = 'white'
 
         # The label of stock's name 
-        name_var = stock_name ###
+        name_var = stock_name 
         self.name_label = customtkinter.CTkLabel(master=self.frame, textvariable=name_var, font = main_label_font, text= name_var, text_color = 'white')
-        self.name_label.place(relx=0.1, rely=0.23, anchor=customtkinter.W)
+        self.name_label.place(relx=0.1, rely=0.3, anchor=customtkinter.W)
 
         # The label of stock's cost 
-        cost_var = stock_cost ###
+        cost_var = stock_cost 
         self.cost_label = customtkinter.CTkLabel(master=self.frame, textvariable=cost_var, font = secondary_label_font, text= cost_var, text_color = actual_color)
         self.cost_label.place(relx=0.1, rely=0.63, anchor=customtkinter.W)   
 
         # The label of stock's raise
-        stock_change_var = '$' + str(stock_change) ###
+        stock_change_var = 'Earnings: $' + str(stock_change) 
         self.change_label = customtkinter.CTkLabel(master=self.frame, textvariable=stock_change_var, font = secondary_label_font, text= stock_change_var, text_color = actual_color)
-        self.change_label.place(relx=0.3, rely=0.83, anchor=customtkinter.W)   
+        self.change_label.place(relx=0.1, rely=0.83, anchor=customtkinter.W)   
         
         # The label of stock's raise %
-        raise_percent_var = str(stock_raise) + '%'###
-        self.raise_percent_label = customtkinter.CTkLabel(master=self.frame, textvariable=raise_percent_var, font = secondary_label_font, text= raise_percent_var, text_color = actual_color)
-        self.raise_percent_label.place(relx=0.1, rely=0.83, anchor=customtkinter.W)   
+        raise_percent_var = str(stock_raise) + '%'
+        self.raise_percent_label = customtkinter.CTkLabel(master=self.frame, textvariable=raise_percent_var, font = main_label_font, text= raise_percent_var, text_color = actual_color)
+        self.raise_percent_label.place(relx=0.9, rely=0.3, anchor=customtkinter.E)   
 
         delete_btn = customtkinter.CTkButton(master=self.frame, text='', command=self.delete_frame,
                                           height=10, width=295, corner_radius = 13, fg_color = 'transparent', hover_color='#9c322a')
         delete_btn.place(relx=0.5, rely=0.98, anchor=customtkinter.CENTER)
 
     # Cоздание экземпляра класса StockFrame при запуске, из файла stocks.json
-    stock_frame = None
-    def restore_frame(entry_str):
-        global stock_frame
-        if entry_str != '':
-            stock_data = get_stock_data(entry_str)
-            stock_cost = '$' + stock_data.get('last_close') + '  ->  ' + '$' + stock_data.get('item_cost')
-            stock_change = round(stock_data.get('change'), 2)
-            stock_raise = round((float(stock_change) / float(stock_data.get('item_cost'))) * 100, 2)
-            stock_frame = StockFrame(main_frame, stock_name=entry_str, stock_cost=stock_cost, stock_change=stock_change, stock_raise=stock_raise)
+    #stock_frame = None
+    #def restore_frame(entry_str):
+    #    global stock_frame
+    #    if entry_str != '':
+    #        stock_data = get_stock_data(entry_str)
+    #        stock_cost = 'Last close: $' + stock_data.get('last_close') + '    ' + 'Now :$' + stock_data.get('item_cost')
+    #        stock_change = round(stock_data.get('change'))
+    #        stock_raise = round((float(stock_change) / float(stock_data.get('item_cost'))) * 100, 2)
+    #        stock_frame = StockFrame(main_frame, stock_name=entry_str, stock_cost=stock_cost, stock_change=stock_change, stock_raise=stock_raise)
 
-    # Создание экземпляра класса StockFrame при вызове функции open_new_frame()
+    # The creation of class example open_new_frame()
     def open_new_frame():
-        global stock_frame
-        entry_str = entry.get()
-        index_choice = combo_box.get()
-        print (index_choice)
-        if entry_str != '':
-            stock_data = get_stock_data(entry_str, index_choice)
-            print (type(stock_data))
-            stock_cost = '$' + stock_data.get('last_close') + '  ->  ' + '$' + stock_data.get('item_cost')
-            print (type(stock_cost))
-            stock_change = round(stock_data.get('change'), 2)
-            stock_raise = round((float(stock_change) / float(stock_data.get('item_cost'))) * 100, 2)
-            stock_frame = StockFrame(main_frame, stock_name=entry_str, stock_cost=stock_cost, stock_change=stock_change, stock_raise=stock_raise)
-            if type(stock_cost) == str: #????#????#????
-                info_label.configure(text = 'Done')#????#????#????
-            else:
-                info_label.configure(text = 'Error') #????#????#????#????
+        try:
+            global stock_frame
+            entry_str = entry.get()
+            index_choice = combo_box.get()
+            if entry_str != '':
+                stock_data = get_stock_data(entry_str, index_choice)
+                stock_cost = 'Close: $' + stock_data.get('last_close') + '          ' + 'Now :$' + stock_data.get('item_cost')
+                stock_change = round(stock_data.get('change'))
+                stock_raise = round((float(stock_change) / float(stock_data.get('item_cost'))) * 100, 2)
+                stock_frame = StockFrame(main_frame, stock_name=entry_str, stock_cost=stock_cost, stock_change=stock_change, stock_raise=stock_raise)
+                info_label.configure(text = 'Done')
+                saving_stock(entry_str, index_choice)
+        except KeyError:
+            info_label.configure(text = 'Error')
    
     # Frame deleting function
     def delete_frame(self):
@@ -133,20 +132,26 @@ entry.place (anchor = customtkinter.SW, relx = 0.05, rely = 0.8)
 combo_box = customtkinter.CTkComboBox(master=bottom_frame, width = 145, values= ['NASDAQ', 'NYSE', 'index3'])
 combo_box.place (anchor = customtkinter.SW, relx = 0.05, rely =0.4)
 
+
+
 # Info-label 
 main_label_font = customtkinter.CTkFont(family="Roboto", size=20, weight="bold")
 status = None  ###
-info_label = customtkinter.CTkLabel(master=bottom_frame, textvariable=status, font = main_label_font, text= status, text_color = 'white')
-info_label.place(anchor = customtkinter.SE, relx = 0.95, rely = 0.4)
 
+# The font color operation for the status
+info_label = customtkinter.CTkLabel(master=bottom_frame, textvariable=status, font = main_label_font, text= status, text_color = 'white')
+info_label.place(anchor = customtkinter.CENTER, relx = 0.73, rely = 0.3)
+
+
+    
 # The window and grid settings
 STOCK_GUI.geometry(str(window_size_x)+'x'+str(window_size_y))
 STOCK_GUI.attributes('-alpha', 0.9)
 STOCK_GUI.grid_rowconfigure(0, weight=1)
 STOCK_GUI.grid_columnconfigure(1, weight=0)
-STOCK_GUI.grid_columnconfigure(0, weight=1) 
+STOCK_GUI.grid_columnconfigure(0, weight=1)
 
-# The reading stocks.json operation if it contains comething
+# The reading stocks.json operation if it contains something
 #with open('stocks.json', mode = 'r', encoding='utf-8') as file:
 #    text = [file.read()]
 #    print (text)
@@ -162,7 +167,5 @@ STOCK_GUI.grid_columnconfigure(0, weight=1)
         
 STOCK_GUI.mainloop()
 
-
-# Сделать параметры создания фрейма
 # Добавить цикл обновления фреймов
 # Добавить чтение фреймов при запуске, если уже имеется файл с настройками фреймов
--- functions.py
+++ functions.py
@@ -1,30 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
-import json
 def get_stock_data(stock_name, index):
-        base_url = "https://www.google.com/finance"
-        company_index = index
-        company_name = stock_name
-        lang = "en"
-        end_url = f"{base_url}/quote/{company_name}:{company_index}?hl={lang}"
-        # Creating na HTTP request
-        page = requests.get(end_url)
-        # Grabbing content using parser
-        soup = BeautifulSoup(page.content, "html.parser")
-        # Packing the description in dictionary
-        stock_description = {}
-        stock_description['item_cost'] = soup.find("div", {"class": "YMlKec fxKbKc"}).text[1:]
-        stock_description['last_close'] = soup.find("div", {"class": "P6K39c"}).text[1:]
-        stock_description['change'] =float(stock_description.get('item_cost')) - float(stock_description.get('last_close'))
-        return stock_description
+    base_url = "https://www.google.com/finance"
+    company_index = index
+    company_name = stock_name
+    lang = "en"
+    end_url = f"{base_url}/quote/{company_name}:{company_index}?hl={lang}"
+    # Creating an HTTP request
+    page = requests.get(end_url)
+    # Grabbing content using parser
+    soup = BeautifulSoup(page.content, "html.parser")
+    # Packing the description in dictionary
+    stock_description = {}
+    stock_description['item_cost'] = soup.find("div", {"class": "YMlKec fxKbKc"}).text[1:]
+    stock_description['last_close'] = soup.find("div", {"class": "P6K39c"}).text[1:]
+    stock_description['change'] =float(stock_description.get('item_cost')) - float(stock_description.get('last_close'))
+    return stock_description
+
 
     # Writing the stock_description to the file
-#    with open('stocks.json', mode = 'r+', encoding='utf-8') as file:
-#        text=json.load()
-#        if stock_name in text:
-#            print('text has ' + stock_name)
-#        else:
-#            file.write('{"stock_name": "' + stock_name + '", "company_index": "' + company_index + '"}' + '\n')
+def saving_stock(stock_name, index):
+    print (stock_name, index)
+    with open('stocks.json', mode = 'r+', encoding='utf-8') as file:
+        text = file.read()
+        if stock_name in text:
+            print('text has ' + stock_name)
+        else:
+            file.write('{"stock_name": "' + stock_name + '", "company_index": "' + index + '"}' + '\n')
 
 
 def index_selection():
